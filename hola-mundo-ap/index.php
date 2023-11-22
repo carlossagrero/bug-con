8c4a1775263505914e7fc78f47d2f829a0fd46d9
carlossagrero <73003723+carlossagrero@users.noreply.github.com>
2023-11-22 10:53:26 -0600

    Add files via upload

---
+++ hola-mundo-ap/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<?php
+    echo "\n";
+    echo "<h1 style='text-align:center'>Bienvenidos</h1>";
+?>
+<div style="margin:0 auto;">
+    <a href="./saludo.php?nombre=Usuario ;-)">Saludo desde el portal!!!</a>
+    <br><br>
+    <img src="./hola-mundo.jpg" alt="">
+</div>
+</html>
---
+++ hola-mundo-ap/saludo.php
@@ -0,0 +1,8 @@
+<?php
+  $nombre = (string) $_GET['nombre'];
+?>
+<!DOCTYPE html>
+  
+<h1> Hola: <?=$nombre?> </h1>
+<a href="./index.php">Regresa a la página principal</a>
+</html>
